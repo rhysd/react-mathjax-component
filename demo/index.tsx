1c87f01056b610ff4543fe5f99b5c708decfb3d3
rhysd <lin90162@yahoo.co.jp>
2022-12-02 16:19:23 +0900

    make `document` prop optional

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { mathjax } from 'mathjax-full/js/mathjax';
-import { TeX } from 'mathjax-full/js/input/tex';
-import { SVG } from 'mathjax-full/js/output/svg';
-import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
 import Mathjax from '..';
 
-const handler = mathjax.document('', {
-    InputJax: new TeX({ packages: AllPackages }),
-    OutputJax: new SVG({ fontCache: 'local' }),
-});
 const DEFAULT_EXPR = '\\left( \\sum_{k=1}^n a_k b_k \\right)^2 \\leq \\left( \\sum_{k=1}^n a_k^2 \\right) \\left( \\sum_{k=1}^n b_k^2 \\right)';
 
 const App: React.FC = () => {
@@ -27,7 +19,7 @@ const App: React.FC = () => {
         <div id="expr-output">
             <label className="label">Rendered</label>
             <div id="expr" className="box">
-                <Mathjax expr={expr} document={handler} />
+                <Mathjax expr={expr} />
             </div>
         </div>
     </>;
--- index.tsx
+++ index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import type { MathDocument } from 'mathjax-full/js/core/MathDocument';
 import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor';
 import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html';
+import { mathjax } from 'mathjax-full/js/mathjax';
+import { TeX } from 'mathjax-full/js/input/tex';
+import { SVG } from 'mathjax-full/js/output/svg';
+import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
 import type { LiteElement } from 'mathjax-full/js/adaptors/lite/Element';
 import type { LiteText } from 'mathjax-full/js/adaptors/lite/Text';
 import type { LiteDocument } from 'mathjax-full/js/adaptors/lite/Document';
@@ -9,16 +13,29 @@ import { render } from './litedom';
 
 export type Document = MathDocument<LiteElement, LiteText, LiteDocument>;
 
-const ADAPTOR = liteAdaptor();
-RegisterHTMLHandler(ADAPTOR);
+RegisterHTMLHandler(liteAdaptor());
+
+let cache: Document | null = null;
+function getDefaultDocument(): Document {
+    if (cache !== null) {
+        return cache;
+    }
+    const document = mathjax.document('', {
+        InputJax: new TeX({ packages: AllPackages }),
+        OutputJax: new SVG({ fontCache: 'local' }),
+    });
+    cache = document;
+    return document;
+}
 
 export interface MathjaxProps {
     expr: string;
-    document: Document;
+    document?: Document;
 }
 
 const Mathjax: React.FC<MathjaxProps> = ({expr, document}) => {
-    const node = document.convert(expr) as LiteElement;
+    const converter = document ?? getDefaultDocument();
+    const node = converter.convert(expr) as LiteElement;
     const children = render(node.children);
     return <>{children}</>;
 };
