4b0c028f00c1915e08f0bec179f71f0ff7740581
rhysd <lin90162@yahoo.co.jp>
2022-12-03 11:32:14 +0900

    make conversion about 1.5x faster
    - before: 3,495 ops/sec
    - after:  5,194 ops/sec

--- litedom.ts
+++ litedom.ts
@@ -1,7 +1,6 @@
 import { createElement } from 'react';
 import type { LiteElement, LiteNode } from 'mathjax-full/js/adaptors/lite/Element';
 import type { LiteText } from 'mathjax-full/js/adaptors/lite/Text';
-import { camelCase } from 'camel-case';
 
 function isLiteText(node: LiteNode): node is LiteText {
     return node.kind === '#text';
@@ -11,33 +10,51 @@ interface Style {
     [name: string]: string;
 }
 
+// Convert cebab-case into camelCase. `camelCase` function from 'camel-case' npm package is slower because it does not
+// know the input is in cebab-case. This function is about 1.5x faster than it.
+const DELIMITER = /[:-]/;
+function camelize(name: string): string {
+    if (!DELIMITER.test(name)) {
+        return name;
+    }
+    const parts = name.split(DELIMITER);
+    for (let i = 1; i < parts.length; i++) {
+        const part = parts[i];
+        if (part.length > 0) {
+            parts[i] = part[0].toUpperCase() + part.slice(1);
+        }
+    }
+    return parts.join('');
+}
+
 // This function will be no longer necessary when this PR is merged.
 // https://github.com/mathjax/MathJax-src/pull/877
 function parseStyle(text: string): Style {
     const style: Style = {};
     for (const s of text.split(';')) {
         const [name, val] = s.split(':');
-        style[camelCase(name)] = val;
+        style[camelize(name)] = val;
     }
     return style;
 }
 
-interface LiteProps {
+interface Props {
+    key: string | number;
     style?: Style;
-    [key: string]: any;
+    [key: string]: unknown;
 }
 
-function props(elem: LiteElement, key: number | string): LiteProps {
-    const props: LiteProps = { key };
+function props(elem: LiteElement, key: number | string): Props {
+    const props: Props = { key };
     let style = '';
     for (const name of Object.keys(elem.attributes)) {
         const value = elem.attributes[name];
-        if (name.startsWith('data-')) {
+        if (name.startsWith('data-') || name.startsWith('aria-')) {
             props[name] = value;
         } else if (name === 'style') {
             style = value;
         } else {
-            props[camelCase(name)] = value;
+            props[camelize(name)] = value;
         }
     }
     if (elem.styles !== null) {
