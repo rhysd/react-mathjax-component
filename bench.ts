cde261f0b4a347533bbec5e4bc5e07a8d4848254
rhysd <lin90162@yahoo.co.jp>
2022-12-11 23:57:31 +0900

    add initial rendering bench using `renderToString`

--- bench.ts
+++ bench.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/naming-convention */
 import Benchmark from 'benchmark';
 import { mathjax } from 'mathjax-full/js/mathjax';
 import { TeX } from 'mathjax-full/js/input/tex';
@@ -7,6 +8,8 @@ import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
 import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor';
 import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html';
 import type { LiteElement } from 'mathjax-full/js/adaptors/lite/Element';
+import { createElement } from 'react';
+import { renderToString } from 'react-dom/server';
 import { render } from './litedom';
 
 const adaptor = liteAdaptor();
@@ -21,11 +24,12 @@ const expr =
     '\\left( \\sum_{k=1}^n a_k b_k \\right)^2 \\leq \\left( \\sum_{k=1}^n a_k^2 \\right) \\left( \\sum_{k=1}^n b_k^2 \\right)';
 const node = document.convert(expr) as LiteElement;
 
+console.log('Running bench for low-layer internal rendering');
 new Benchmark.Suite()
-    .add('mathjax::stringify', function () {
+    .add('render::mathjax_stringify', function () {
         adaptor.innerHTML(node);
     })
-    .add('react::render', function () {
+    .add('render::react_through_litedom', function () {
         render(node.children);
     })
     .on('cycle', function (event: Event) {
@@ -34,5 +38,23 @@ new Benchmark.Suite()
     .on('complete', function (this: Benchmark.Suite) {
         console.log(`Fastest is ${this.filter('fastest').map('name')}`);
     })
-    // run async
-    .run({ async: true });
+    .run();
+
+console.log('\nRunning bench for initial React DOM rendering');
+new Benchmark.Suite()
+    .add('initial::mathjax_through_string', function () {
+        const html = { __html: adaptor.innerHTML(node) };
+        const elem = createElement('span', { dangerouslySetInnerHTML: html });
+        renderToString(elem);
+    })
+    .add('initial::react_through_litedom', function () {
+        const elem = createElement('span', null, ...render(node.children));
+        renderToString(elem);
+    })
+    .on('cycle', function (event: Event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function (this: Benchmark.Suite) {
+        console.log(`Fastest is ${this.filter('fastest').map('name')}`);
+    })
+    .run();
